dc665b60d66a184caaa4b4140613db529fe11940
Christian Arcos <dayan3846@gmail.com>
2020-10-20 00:03:03 -0500

    data anda models

---
+++ models/unet.py
@@ -0,0 +1,119 @@
+import torch.nn as nn
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from .common import * 
+
+class ListModule(nn.Module):
+    def __init__(self, *args):
+        super(ListModule, self).__init__()
+        idx = 0
+        for module in args:
+            self.add_module(str(idx), module)
+            idx += 1
+
+    def __getitem__(self, idx):
+        if idx >= len(self._modules):
+            raise IndexError('index {} is out of range'.format(idx))
+        if idx < 0: 
+            idx = len(self) + idx
+
+        it = iter(self._modules.values())
+        for i in range(idx):
+            next(it)
+        return next(it)
+
+    def __iter__(self):
+        return iter(self._modules.values())
+
+    def __len__(self):
+        return len(self._modules)
+
+class UNet(nn.Module):
+    '''
+        upsample_mode in ['deconv', 'nearest', 'bilinear']
+        pad in ['zero', 'replication', 'none']
+    '''
+    def __init__(self, num_input_channels=3, num_output_channels=3, 
+                       feature_scale=4, more_layers=0, concat_x=False,
+                       upsample_mode='deconv', pad='zero', norm_layer=nn.InstanceNorm2d, need_sigmoid=True, need_bias=True):
+        super(UNet, self).__init__()
+
+        self.feature_scale = feature_scale
+        self.more_layers = more_layers
+        self.concat_x = concat_x
+
+
+        filters = [64, 128, 256, 512, 1024]
+        filters = [x // self.feature_scale for x in filters]
+
+        self.start = unetConv2(num_input_channels, filters[0] , norm_layer, need_bias, pad)
+
+        self.down1 = unetDown(filters[0], filters[1] , norm_layer, need_bias, pad)
+        self.down2 = unetDown(filters[1], filters[2] , norm_layer, need_bias, pad)
+        #self.down3 = unetDown(filters[2], filters[3] , norm_layer, need_bias, pad)
+        #self.down4 = unetDown(filters[3], filters[4] , norm_layer, need_bias, pad)
+
+        self.final = conv(filters[2], num_output_channels, 1, bias=need_bias, pad=pad)
+
+        if need_sigmoid: 
+            self.final = nn.Sequential(self.final, nn.Sigmoid())
+
+    def forward(self, inputs):
+
+        # Downsample 
+        downs = [inputs]
+        down = nn.AvgPool2d(2, 2)
+        for i in range(2 + self.more_layers):
+            downs.append(down(downs[-1]))
+
+        in64 = self.start(inputs)
+
+        down1 = self.down1(in64)
+
+        down2 = self.down2(down1)
+
+        #down3 = self.down3(down2)
+
+        #down4 = self.down4(down3)
+
+        #print(down2.shape)
+        return self.final(down2)
+
+
+
+class unetConv2(nn.Module):
+    def __init__(self, in_size, out_size, norm_layer, need_bias, pad):
+        super(unetConv2, self).__init__()
+
+        print(pad)
+        if norm_layer is not None:
+            self.conv1= nn.Sequential(conv(in_size, out_size, 3, bias=need_bias, pad=pad),
+                                       norm_layer(out_size),
+                                       nn.ReLU(),)
+            self.conv2= nn.Sequential(conv(out_size, out_size, 3, bias=need_bias, pad=pad),
+                                       norm_layer(out_size),
+                                       nn.ReLU(),)
+        else:
+            self.conv1= nn.Sequential(conv(in_size, out_size, 3, bias=need_bias, pad=pad),
+                                       nn.ReLU(),)
+            self.conv2= nn.Sequential(conv(out_size, out_size, 3, bias=need_bias, pad=pad),
+                                       nn.ReLU(),)
+    def forward(self, inputs):
+        outputs= self.conv1(inputs)
+        outputs= self.conv2(outputs)
+        return outputs
+
+
+class unetDown(nn.Module):
+    def __init__(self, in_size, out_size, norm_layer, need_bias, pad):
+        super(unetDown, self).__init__()
+        self.conv= unetConv2(in_size, out_size, norm_layer, need_bias, pad)
+        self.down= nn.MaxPool2d(2, 2)
+
+    def forward(self, inputs):
+        outputs= self.down(inputs)
+        outputs= self.conv(outputs)
+        return outputs
+
+
